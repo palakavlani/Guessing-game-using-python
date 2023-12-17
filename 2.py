47ca26430e92dbe4069c5242188e41bd343d243d
palakavlani <122960204+palakavlani@users.noreply.github.com>
2023-12-17 18:40:00 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,20 @@
+import random
+number = random.randint(1, 10)
+number_of_guesses=0
+player_name = input("Hello, What's your name?")
+print('okay! '+ player_name+ ' Guess a number between 1 and 10:')
+while number_of_guesses < 5:
+        guess = int(input())
+        number_of_guesses += 1
+
+        if guess < number:
+            print('Your guess is too low.')
+        elif guess > number:
+            print('Your guess is too high.')
+        else:
+            break
+if guess == number:
+    print('Congratulations, ' + player_name + '! You guessed the number in ' + str(number_of_guesses) + ' tries!')
+else:
+    print('Sorry, ' + player_name + ', you did not guess the number. The number was ' + str(number))
+"\n "
